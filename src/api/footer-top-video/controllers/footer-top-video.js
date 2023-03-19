5aa70370e3d0945ed65b29f76669ac547dafccc9
Yunus <52599021+Noctis-jpg@users.noreply.github.com>
2023-03-20 01:25:30 +0300

    Home Page bitmek üzere Footer Kaldı
    AboutHome Text Home FooterTopvideo ve bir çok alan tamamlandı.

---
+++ src/api/footer-top-video/controllers/footer-top-video.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * footer-top-video controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::footer-top-video.footer-top-video');
---
+++ src/api/footer-top-video/routes/footer-top-video.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * footer-top-video router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::footer-top-video.footer-top-video');
